a6039be9ec6f356bb064d608a6e2241714b95f15
ggualbertosouza <contatoguilhermegsz@gmail.com>
2023-06-01 00:56:51 -0300

    add client page and some funcionts

---
+++ mechanic_system/clientes/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def clientes(request):
+    if request.method == "GET":
+        return render(request, 'clientes.html')
+    elif request.method == "POST":
+        nome = request.POST.get('nome')
+        sobrenome = request.POST.get('sobrenome')
+        email = request.POST.get('email')
+        cpf = request.POST.get('cpf')
+        carros = request.POST.getlist('carro')
+        placas = request.POST.getlist('placa')
+        anos = request.POST.getlist('ano')
+
+        print(nome)
+        print(carros, placas, anos)
+        return HttpResponse('teste')
